cbcd4eb1b3e80c7608bc5fa5d32bc6036d7ab038
jhessicarauj <jessica.8956@aluno.pr.senac.br>
2024-03-11 11:56:54 -0300

    projeto finalizado sem banco de dados

---
+++ src/database/knex/migrations/20240311142631_createUsers.js
@@ -0,0 +1,18 @@
+exports.up = (knex) => { return knex.schema.createTable("users", (table) => { 
+
+    table.increments('id').primary(); 
+    table.string("name").notNullable(); 
+    table.string("username").notNullable(); 
+    table.string("email").notNullable(); 
+    table.string("telefone").notNullable(); 
+    table.booleans("isAdmin").defautTo("false") 
+
+}) 
+  
+};
+
+
+exports.down = (knex) => {
+    return knex.schema.droptableIfExists("users")
+  
+};
---
+++ src/database/knex/migrations/20240311143501_createBook.js
@@ -0,0 +1,16 @@
+exports.up = (knex) => { return knex.schema.createTable("books", (table) => {
+    table.increments('id').primary();
+    table.string("titulo").notNullable();
+    table.string("autor").notNullable();
+    table.string("paginas").notNullable();
+    table.string("categoria").notNullable();
+    table.booleans("disponibilidade").defautTo("true")
+})
+  
+};
+
+
+exports.down = (knex) => {
+    return knex.schema.droptableIfExists("books")
+  
+};
