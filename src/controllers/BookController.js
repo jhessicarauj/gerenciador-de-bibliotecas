8994950407621675c09e42e76f741849ec3a7924
jhessicarauj <jessica.8956@aluno.pr.senac.br>
2024-03-11 09:27:43 -0300

    projeto finalizado sem banco de dados

---
+++ src/controllers/BookController.js
@@ -0,0 +1,62 @@
+const knex = require("../database/knex")
+
+class BookController {
+    async createBook(req, res) {
+        const {id} = req.params
+        const {titulo , autor, paginas, categoria} = req.body
+
+        const book = {
+            id,
+            titulo,
+            autor,
+            paginas,
+            categoria,
+            created_at: `${new Date()}`,
+            updated_at: `${new Date()}`,
+            disponibilidade: true
+        }
+
+        await knex ("books").insert({titulo: book.titulo, autor: book.autor, paginas: book.paginas, categoria: book.categoria })
+        return res.status(201).json("Livro criado com sucesso")
+    }
+
+    async listbook(req, res) {
+        const [books] = await knex("books")
+
+        return res.status(200).json(books)
+
+    }
+
+    async listBookByld(req, res) {
+        const {id} = req.params
+        const [book] = await knex("books").where({id})
+
+         return res.status(200).json(book)
+
+    }
+
+    async updateTask(req, res) {
+        const {id} = req.params
+        const {titulo, categoria} = req.body
+
+        const newTask = await knex ("books").where({id}).update({titulo, categoria})
+
+        return res.status(200).json("Registro atualizado com sucesso!")
+    }
+
+    async updateBookStatus(req, res) {
+        const {id} = req.params
+        
+        await knex("books").where({id}).update({disponibilidade: true})
+
+        return res.status(200).json("Status atualizado com sucesso!")
+
+    }
+
+    async deleteBook (req, res) {
+        const {id} = req.params
+        await knex("books").where({id}).delete({id})
+
+        return res.status(200).json("Registro deletado com sucesso!")
+    }
+}
