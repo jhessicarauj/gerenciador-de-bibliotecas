3a06388bfb94bb6d86105375268602eb91172c17
jhessicarauj <jessica.8956@aluno.pr.senac.br>
2024-03-14 10:07:16 -0300

    subindo arquivo de banco de dados

--- src/database/knex/migrations/20240314124845_createEmprestimos.js
+++ src/database/knex/migrations/20240314124845_createEmprestimos.js
@@ -1,7 +1,7 @@
 
 exports.up = (knex) => {
     return knex.schema.createTable("loans", (table) => {
-        table.increments("id").primay();
+        table.increments("id").primary();
         table.integer("user_id").unsigned().index().references("id").inTable("users");
         
         table.integer("book_id").unsigned().index().references("id").inTable("books");
